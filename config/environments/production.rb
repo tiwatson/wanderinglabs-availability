f62547a1307aeb385b09c5a69e7153a9fabf3123
Tim Watson <tiwatson@gmail.com>
2015-01-01 20:40:45 -0500

    Sendgrind for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  Aconfig.action_mailer.smtp_settings = {
+    :user_name => Rails.application.secrets.sendgrid_username,
+    :password => Rails.application.secrets.sendgrid_password,
+    :domain => 'wanderinglabs.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
