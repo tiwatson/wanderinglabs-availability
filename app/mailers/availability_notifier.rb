901f95c60a7d7da36c32083d6e9e1f96d01c7e60
Tim Watson <tiwatson@gmail.com>
2015-01-02 22:01:47 -0500

    Notification email tweaks

--- app/mailers/availability_notifier.rb
+++ app/mailers/availability_notifier.rb
@@ -4,6 +4,10 @@ class AvailabilityNotifier < ActionMailer::Base
   def notify(availability_request_id)
     @availability_request = AvailabilityRequest.find availability_request_id
     @ar = @availability_request
+
+    @availabilities_new = @ar.availabilities.is_available.to_notify.order(:date_start)
+    @availabilities_past = @ar.availabilities.is_available.where('notified_at is not null').order(:date_start)
+
     mail(to: @availability_request.user.email, subject: "Site(s) are available at #{@ar.location.name}")
   end
 
