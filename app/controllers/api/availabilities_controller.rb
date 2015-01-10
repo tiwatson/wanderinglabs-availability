6c9930d2cdaba28180b80793934d99d632556538
Tim Watson <tiwatson@gmail.com>
2015-01-10 11:32:08 -0500

    Availability count in request serializer

--- app/controllers/api/availabilities_controller.rb
+++ app/controllers/api/availabilities_controller.rb
@@ -1,7 +1,7 @@
 class Api::AvailabilitiesController < ApplicationController
 
   def index
-    a = Availability.where(available: true).all
+    a = Availability.where(available: true).order(:id).all
     render status: 200, json: a, root: false
   end
 
--- app/serializers/availability_request_serializer.rb
+++ app/serializers/availability_request_serializer.rb
@@ -1,5 +1,14 @@
 class AvailabilityRequestSerializer < ActiveModel::Serializer
 
-  attributes :id, :date_start, :date_end, :days_length, :active, :last_checked, :expired
+  attributes :id, :last_checked, :date_start, :date_end, :days_length, :active, :expired, :availabilities_count, :availabilities_active_count
   has_one :location
+
+  def availabilities_count
+    object.availabilities.count
+  end
+
+  def availabilities_active_count
+    object.availabilities.is_available.count
+  end
+
 end
