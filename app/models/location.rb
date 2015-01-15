19d2afee6477c5c109d31c70ff107dd072085866
Tim Watson <tiwatson@gmail.com>
2015-01-14 19:49:03 -0500

    Contract Code on Location Url

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,9 +2,9 @@ class Location < ActiveRecord::Base
 
   has_many :availability_requests
   belongs_to :location_agency
-  
+
   def url
-    "http://www.reserveamerica.com/camping/#{slug}/r/campgroundDetails.do?contractCode=#{state}&parkId=#{park_id}"
+    "http://www.reserveamerica.com/camping/#{slug}/r/campgroundDetails.do?contractCode=#{contract_code}&parkId=#{park_id}"
   end
 
 end
